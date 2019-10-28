adb60e6783c58c26e192af4d768c6f89b1d837c3
Steve Bauman <steven_bauman@outlook.com>
2019-10-28 11:52:42 -0400

    Fixed trait.

--- src/Traits/HasLdapUser.php
+++ src/Traits/HasLdapUser.php
@@ -2,23 +2,23 @@
 
 namespace LdapRecord\Laravel\Traits;
 
-use Adldap\Models\User;
+use LdapRecord\Models\Model;
 
 trait HasLdapUser
 {
     /**
      * The LDAP User that is bound to the current model.
      *
-     * @var User|null
+     * @var Model|null
      */
     public $ldap;
 
     /**
      * Sets the LDAP user that is bound to the model.
      *
-     * @param User $user
+     * @param Model $user
      */
-    public function setLdapUser(User $user = null)
+    public function setLdapUser(Model $user = null)
     {
         $this->ldap = $user;
     }
