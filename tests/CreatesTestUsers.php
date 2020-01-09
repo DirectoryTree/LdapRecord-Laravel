92e7262eef1b06e3f49bb212961624a66ce9d30f
Steve Bauman <steven_bauman@outlook.com>
2020-01-09 11:48:53 -0500

    Allow passing a custom model

--- tests/CreatesTestUsers.php
+++ tests/CreatesTestUsers.php
@@ -4,9 +4,11 @@ namespace LdapRecord\Laravel\Tests;
 
 trait CreatesTestUsers
 {
-    protected function createTestUser(array $attributes = [])
+    protected function createTestUser(array $attributes = [], $model = null)
     {
-        return tap(new TestUser, function (TestUser $user) use ($attributes) {
+        $model = $model ?? TestUser::class;
+
+        return tap(new $model, function (TestUser $user) use ($attributes) {
             foreach ($attributes as $name => $value) {
                 $user->{$name} = $value;
             }
