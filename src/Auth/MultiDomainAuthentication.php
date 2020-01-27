0cb76400a55ff77fffcf7fec4a4dcdb3e49d6e39
Steve Bauman <steven_bauman@outlook.com>
2020-01-26 19:57:43 -0500

    Working on multi-domain auth trait for LoginController

---
+++ src/Auth/MultiDomainAuthentication.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LdapRecord\Laravel\Auth;
+
+use LdapRecord\Models\Model;
+use Illuminate\Support\Facades\Auth;
+
+trait MultiDomainAuthentication
+{
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function getLdapGuard()
+    {
+        return Auth::guard(
+            Auth::check() ? $this->getLdapGuardFromUser() : $this->getLdapGuardFromRequest()
+        );
+    }
+
+    /**
+     * Get the LDAP domain from the request.
+     *
+     * @return string|null
+     */
+    protected function getLdapGuardFromRequest()
+    {
+        return request('domain') ?? config('ldap.default');
+    }
+
+    /**
+     * Get the LDAP domain from the authenticated user.
+     *
+     * @return string|null
+     */
+    protected function getLdapGuardFromUser()
+    {
+        $user = Auth::user();
+
+        if ($user instanceof Model) {
+            return $user->getConnectionName() ?? config('ldap.default');
+        }
+
+        if ($user instanceof LdapAuthenticatable) {
+            return $user->getLdapDomain();
+        }
+
+        return;
+    }
+}
