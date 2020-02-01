610bb249637cf43c839bbc91b8e92fb6347e6099
Steve Bauman <steven_bauman@outlook.com>
2020-02-01 09:58:20 -0500

    Disable logging during testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -45,6 +45,7 @@ abstract class TestCase extends BaseTestCase
         ]);
 
         // LDAP mock setup.
+        $config->set('ldap.logging', false);
         $config->set('ldap.default', 'default');
         $config->set('ldap.connections.default', [
             'hosts' => ['localhost'],
