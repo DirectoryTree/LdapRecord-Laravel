396d21268a3af63b1c978279667dce61530a0749
Steve Bauman <steven_bauman@outlook.com>
2020-02-05 08:39:23 -0500

    Removed DenyTrashed rule - this is done via the authenticator

--- tests/ValidatorRuleTest.php
+++ tests/ValidatorRuleTest.php
@@ -2,25 +2,12 @@
 
 namespace LdapRecord\Laravel\Tests;
 
-use LdapRecord\Laravel\Auth\Rules\DenyTrashed;
 use LdapRecord\Laravel\Auth\Rules\OnlyImported;
 use LdapRecord\Models\Entry;
 
 class ValidatorRuleTest extends TestCase
 {
     use CreatesTestUsers;
-
-    public function test_deny_trashed()
-    {
-        $user = $this->createTestUser([
-            'name' => 'John Doe',
-            'email' => 'jdoe@email.com',
-            'password' => 'password',
-            'deleted_at' => now(),
-        ]);
-
-        $this->assertFalse((new DenyTrashed(new Entry, $user))->isValid());
-    }
 
     public function test_only_imported()
     {
