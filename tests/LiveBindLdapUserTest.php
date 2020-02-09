24f0378ef721e0d636ceed7a31e7c6e47faf1723
Steve Bauman <steven_bauman@outlook.com>
2020-02-09 15:50:36 -0500

    Update LiveBindLdapUserTest.php

--- tests/LiveBindLdapUserTest.php
+++ tests/LiveBindLdapUserTest.php
@@ -7,7 +7,7 @@ use LdapRecord\Laravel\Testing\DirectoryEmulator;
 use LdapRecord\Models\ActiveDirectory\User;
 use Ramsey\Uuid\Uuid;
 
-class BindLdapUserTest extends TestCase
+class LiveBindLdapUserTest extends TestCase
 {
     public function test_ldap_users_are_bound_to_models_using_trait()
     {
@@ -32,5 +32,23 @@ class BindLdapUserTest extends TestCase
 
         $this->assertInstanceOf(User::class, $model->ldap);
         $this->assertTrue($user->is($model->ldap));
+    }
+
+    public function test_ldap_users_are_not_bound_when_model_cannot_be_located()
+    {
+        $this->setupDatabaseUserProvider();
+
+        DirectoryEmulator::setup();
+
+        $model = TestUser::create([
+            'name' => 'John',
+            'email' => 'jdoe@email.com',
+            'guid' => Uuid::uuid4()->toString(),
+            'password' => 'secret',
+        ]);
+
+        Auth::login($model);
+
+        $this->assertNull($model->ldap);
     }
 }
