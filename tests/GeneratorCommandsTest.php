48477e26cef04357214bcfd2683ff04545ae8d6b
Steve Bauman <steven_bauman@outlook.com>
2020-02-14 16:29:18 -0500

    Try to fix generator tests

--- tests/GeneratorCommandsTest.php
+++ tests/GeneratorCommandsTest.php
@@ -7,25 +7,18 @@ class GeneratorCommandsTest extends TestCase
     public function test_make_ldap_model_works()
     {
         $this->artisan('make:ldap-model User')->assertExitCode(0);
-
-        $this->assertFileExists(app_path('Ldap'.DIRECTORY_SEPARATOR.'User.php'));
+        $this->assertFileExists(base_path('App/Ldap/User.php'));
     }
 
     public function test_make_ldap_rule_works()
     {
         $this->artisan('make:ldap-rule OnlyAdmins')->assertExitCode(0);
-
-        $path = 'Ldap'.DIRECTORY_SEPARATOR.'Rules'.DIRECTORY_SEPARATOR.'OnlyAdmins.php';
-
-        $this->assertFileExists(app_path($path));
+        $this->assertFileExists(base_path('App/Ldap/Rules/OnlyAdmins.php'));
     }
 
     public function test_make_ldap_scope_works()
     {
         $this->artisan('make:ldap-scope CompanyScope')->assertExitCode(0);
-
-        $path = 'Ldap'.DIRECTORY_SEPARATOR.'Scopes'.DIRECTORY_SEPARATOR.'CompanyScope.php';
-
-        $this->assertFileExists(app_path($path));
+        $this->assertFileExists(base_path('App/Ldap/Scopes/CompanyScope.php'));
     }
 }
