8aade8fe0bcb1e5a181c1d9bd72e628d7d8f20fe
Steve Bauman <steven_bauman@outlook.com>
2020-03-02 21:08:40 -0500

    Update errors.php

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'user_not_found' => 'User not found',
-    'user_not_permitted_at_this_time' => 'Not permitted to logon at this time',
-    'user_not_permitted_to_login' => 'Not permitted to logon at this workstation',
-    'password_expired' => 'Password expired',
-    'account_disabled' => 'Account disabled',
-    'account_expired' => 'Account expired',
-    'user_must_reset_password' => 'User must reset password',
-    'user_account_locked' => 'User account locked',
+    'user_not_found' => 'User not found.',
+    'user_not_permitted_at_this_time' => 'Not permitted to logon at this time.',
+    'user_not_permitted_to_login' => 'Not permitted to logon at this workstation.',
+    'password_expired' => 'Your password has expired.',
+    'account_disabled' => 'Your account is disabled.',
+    'account_expired' => 'Your account has expired.',
+    'user_must_reset_password' => 'You must reset your password before logging in.',
+    'user_account_locked' => 'Your account is locked.',
 ];
