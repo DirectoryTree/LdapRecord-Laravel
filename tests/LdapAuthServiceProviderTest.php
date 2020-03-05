ad6ee8fe5395dffddcf374ae3244824ef9f84776
Steve Bauman <steven_bauman@outlook.com>
2020-03-05 11:41:43 -0500

    Added some service provider tests

---
+++ tests/LdapAuthServiceProviderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace LdapRecord\Laravel\Tests;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Events\Dispatcher;
+use LdapRecord\Laravel\Events\Importing;
+use LdapRecord\Laravel\Events\Imported;
+use LdapRecord\Laravel\Events\Synchronized;
+use LdapRecord\Laravel\Events\Synchronizing;
+use LdapRecord\Laravel\Events\Authenticated;
+use LdapRecord\Laravel\Events\Authenticating;
+use LdapRecord\Laravel\Events\AuthenticationFailed;
+use LdapRecord\Laravel\Events\AuthenticationRejected;
+use LdapRecord\Laravel\Events\DiscoveredWithCredentials;
+use LdapRecord\Laravel\Events\AuthenticatedWithWindows;
+use LdapRecord\Laravel\Events\AuthenticatedModelTrashed;
+use LdapRecord\Laravel\LdapAuthServiceProvider;
+
+class LdapAuthServiceProviderTest extends TestCase
+{
+    protected function getEnvironmentSetup($app)
+    {
+        parent::getEnvironmentSetup($app);
+
+        $app['config']->set('ldap.logging', true);
+    }
+
+    public function test_migrations_are_publishable()
+    {
+        $this->artisan('vendor:publish', ['--provider' => LdapAuthServiceProvider::class, '--no-interaction' => true]);
+
+        $this->assertFileExists(database_path('migrations/'.date('Y_m_d_His', time()).'_add_ldap_columns_to_users_table.php'));
+    }
+
+    public function test_model_binding_listener_is_setup()
+    {
+        /** @var Dispatcher $event */
+        $dispatcher = app(Dispatcher::class);
+
+        $this->assertTrue($dispatcher->hasListeners(Login::class));
+        $this->assertTrue($dispatcher->hasListeners(Authenticated::class));
+    }
+
+    public function test_event_listeners_are_setup_when_logging_is_enabled()
+    {
+        /** @var Dispatcher $event */
+        $dispatcher = app(Dispatcher::class);
+
+        $events = [
+            Importing::class,
+            Imported::class,
+            Synchronized::class,
+            Synchronizing::class,
+            Authenticated::class,
+            Authenticating::class,
+            AuthenticationFailed::class,
+            AuthenticationRejected::class,
+            DiscoveredWithCredentials::class,
+            AuthenticatedWithWindows::class,
+            AuthenticatedModelTrashed::class,
+        ];
+
+        foreach ($events as $event) {
+            $this->assertTrue($dispatcher->hasListeners($event));
+        }
+    }
+}
--- tests/LdapServiceProviderTest.php
+++ tests/LdapServiceProviderTest.php
@@ -4,6 +4,7 @@ namespace LdapRecord\Laravel\Tests;
 
 use Illuminate\Log\LogManager;
 use LdapRecord\Container;
+use LdapRecord\Laravel\LdapServiceProvider;
 use LdapRecord\Query\Cache;
 
 class LdapServiceProviderTest extends TestCase
@@ -15,6 +16,13 @@ class LdapServiceProviderTest extends TestCase
         $app['config']->set('ldap.logging', true);
         $app['config']->set('ldap.cache.enabled', true);
         $app['config']->set('ldap.cache.driver', 'array');
+    }
+
+    public function test_config_is_publishable()
+    {
+        $this->artisan('vendor:publish', ['--provider' => LdapServiceProvider::class, '--no-interaction' => true]);
+
+        $this->assertFileExists(config_path('ldap.php'));
     }
 
     public function test_logger_is_set_on_container_when_enabled()
