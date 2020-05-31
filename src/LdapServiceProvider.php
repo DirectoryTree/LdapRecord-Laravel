1c35ab80c12672e99c7d65309ae28991a43e23b6
Steve Bauman <steven_bauman@outlook.com>
2020-05-31 14:28:21 -0400

    Make the LDAP service provider more modular for Lumen integration
    https://github.com/DirectoryTree/LdapRecord-Discussions/issues/3

--- src/LdapServiceProvider.php
+++ src/LdapServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace LdapRecord\Laravel;
 
-use Illuminate\Support\ServiceProvider;
-use LdapRecord\Connection;
 use LdapRecord\Container;
+use LdapRecord\Connection;
+use Illuminate\Support\ServiceProvider;
 use LdapRecord\Laravel\Commands\MakeLdapModel;
 use LdapRecord\Laravel\Commands\MakeLdapRule;
 use LdapRecord\Laravel\Commands\MakeLdapScope;
@@ -19,12 +19,36 @@ class LdapServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this
+            ->registerConfiguration()
+            ->registerCommands()
+            ->registerLogging()
+            ->registerLdapConnections();
+    }
+
+    /**
+     * Register the publishable LDAP configuration file.
+     *
+     * @return $this
+     */
+    protected function registerConfiguration()
+    {
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/ldap.php' => config_path('ldap.php'),
             ]);
         }
 
+        return $this;
+    }
+
+    /**
+     * Register the LDAP artisan commands.
+     *
+     * @return $this
+     */
+    protected function registerCommands()
+    {
         $this->commands([
             MakeLdapRule::class,
             MakeLdapScope::class,
@@ -32,22 +56,54 @@ class LdapServiceProvider extends ServiceProvider
             TestLdapConnection::class,
         ]);
 
+        return $this;
+    }
+
+    /**
+     * Register the LDAP operation logger.
+     *
+     * @return $this
+     */
+    protected function registerLogging()
+    {
         if (config('ldap.logging', true)) {
             Container::getInstance()->setLogger(logger());
         }
 
+        return $this;
+    }
+
+    /**
+     * Register the configured LDAP connections.
+     *
+     * @return $this
+     */
+    protected function registerLdapConnections()
+    {
         Container::setDefaultConnection(config('ldap.default', 'default'));
 
         foreach (config('ldap.connections', []) as $name => $config) {
             $connection = new Connection($config);
 
             if (config('ldap.cache.enabled', false)) {
-                $connection->setCache(
-                    cache()->driver(config('ldap.cache.driver'))
-                );
+                $this->registerLdapCache($connection);
             }
 
             Container::addConnection($connection, $name);
         }
+
+        return $this;
+    }
+
+    /**
+     * Register the LDAP cache store on the given connection.
+     *
+     * @param Connection $connection
+     */
+    protected function registerLdapCache(Connection $connection)
+    {
+        $connection->setCache(
+            cache()->driver(config('ldap.cache.driver'))
+        );
     }
 }
