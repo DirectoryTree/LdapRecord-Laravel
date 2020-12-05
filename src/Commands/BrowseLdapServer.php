f6813b982d40fe2836eca1525b38c20a2c67c1c5
Steve Bauman <steven_bauman@outlook.com>
2020-12-05 16:38:24 -0500

    Remove accidental quote in signature

--- src/Commands/BrowseLdapServer.php
+++ src/Commands/BrowseLdapServer.php
@@ -19,7 +19,7 @@ class BrowseLdapServer extends Command
      *
      * @var string
      */
-    protected $signature = 'ldap:browse {connection=default : The LDAP connection to browse.}"';
+    protected $signature = 'ldap:browse {connection=default : The LDAP connection to browse.}';
 
     /**
      * The LDAP connections base DN (root).
