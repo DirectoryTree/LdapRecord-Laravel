72ced434a46a3ac2b68cff634386c7358b44da06
Steve Bauman <steven_bauman@outlook.com>
2021-05-05 23:48:02 -0400

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,34 +1,34 @@
 <?php
 
-$finder = Symfony\Component\Finder\Finder::create()
+$finder = (new Symfony\Component\Finder\Finder)
     ->in([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     ->name('*.php')
     ->ignoreVCS(true)
     ->ignoreDotFiles(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config)
     ->setRules([
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => [
             'default' => 'single_space',
-            'operators' => ['=>' => null]
+            'operators' => ['=>' => null],
         ],
         'blank_line_after_namespace' => true,
         'blank_line_after_opening_tag' => true,
         'blank_line_before_statement' => [
-            'statements' => ['return']
+            'statements' => ['return'],
         ],
         'braces' => true,
         'cast_spaces' => true,
         'class_attributes_separation' => [
-            'elements' => ['method']
+            'elements' => ['method'],
         ],
         'class_definition' => true,
         'concat_space' => [
-            'spacing' => 'none'
+            'spacing' => 'none',
         ],
         'declare_equal_normalize' => true,
         'elseif' => true,
@@ -58,7 +58,7 @@ return PhpCsFixer\Config::create()
                 'throw',
                 'use',
                 'use_trait',
-            ]
+            ],
         ],
         'no_blank_lines_after_class_opening' => true,
         'no_blank_lines_after_phpdoc' => true,
@@ -68,11 +68,11 @@ return PhpCsFixer\Config::create()
         'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_mixed_echo_print' => [
-            'use' => 'echo'
+            'use' => 'echo',
         ],
         'no_multiline_whitespace_around_double_arrow' => true,
         'multiline_whitespace_before_semicolons' => [
-            'strategy' => 'no_multi_line'
+            'strategy' => 'no_multi_line',
         ],
         'no_short_bool_cast' => true,
         'no_singleline_whitespace_before_semicolons' => true,
@@ -119,7 +119,7 @@ return PhpCsFixer\Config::create()
         'single_import_per_statement' => true,
         'single_line_after_imports' => true,
         'single_line_comment_style' => [
-            'comment_types' => ['hash']
+            'comment_types' => ['hash'],
         ],
         'single_quote' => true,
         'space_after_semicolon' => true,
@@ -131,7 +131,7 @@ return PhpCsFixer\Config::create()
         'trim_array_spaces' => true,
         'unary_operator_spaces' => true,
         'visibility_required' => [
-            'elements' => ['method', 'property']
+            'elements' => ['method', 'property'],
         ],
         'whitespace_after_comma_in_array' => true,
     ])->setFinder($finder);
