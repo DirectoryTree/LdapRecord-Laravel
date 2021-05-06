18826484364c3de7f6f6d42d555e5cea195ca797
Steve Bauman <steven_bauman@outlook.com>
2021-05-05 23:55:02 -0400

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -129,7 +129,7 @@ return (new PhpCsFixer\Config)
         'switch_case_semicolon_to_colon' => true,
         'switch_case_space' => true,
         'ternary_operator_spaces' => true,
-        'trailing_comma_in_multiline' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'trim_array_spaces' => true,
         'unary_operator_spaces' => true,
         'visibility_required' => [
