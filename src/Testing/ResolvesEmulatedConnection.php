948fca89fe5c9c23ff572d959a4bda5d2dfe4b11
Steve Bauman <steven_bauman@outlook.com>
2022-09-08 09:03:25 -0400

    Add missing code doc

--- src/Testing/ResolvesEmulatedConnection.php
+++ src/Testing/ResolvesEmulatedConnection.php
@@ -4,6 +4,13 @@ namespace LdapRecord\Laravel\Testing;
 
 trait ResolvesEmulatedConnection
 {
+    /**
+     * Resolve the emulator database connection.
+     *
+     * @param string $connection
+     *
+     * @return \Illuminate\Database\Connection
+     */
     public static function resolveConnection($connection = null)
     {
         return app(LdapDatabaseManager::class)->connection($connection);
