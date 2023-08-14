46826d62db42cb33d97a67ad23cc8ff56f753c00
Hugh Messenger <hugh.messenger@gmail.com>
2023-08-14 14:11:50 -0500

    Fixing remove() method in emulator, added test for it

--- src/Testing/Emulated/EmulatesModelQueries.php
+++ src/Testing/Emulated/EmulatesModelQueries.php
@@ -30,7 +30,7 @@ trait EmulatesModelQueries
      */
     public function add(string $dn, array $attributes): bool
     {
-        if (! $model = $this->find($dn)) {
+        if (!$model = $this->find($dn)) {
             return false;
         }
 
@@ -48,7 +48,7 @@ trait EmulatesModelQueries
      */
     public function update(string $dn, array $modifications): bool
     {
-        if (! $model = $this->findEloquentModelByDn($dn)) {
+        if (!$model = $this->findEloquentModelByDn($dn)) {
             return false;
         }
 
@@ -64,7 +64,7 @@ trait EmulatesModelQueries
      */
     public function replace(string $dn, array $attributes): bool
     {
-        if (! $model = $this->find($dn)) {
+        if (!$model = $this->find($dn)) {
             return false;
         }
 
@@ -82,16 +82,16 @@ trait EmulatesModelQueries
      */
     public function remove(string $dn, array $attributes): bool
     {
-        if (! $model = $this->find($dn)) {
+        if (!$model = $this->find($dn)) {
             return false;
         }
 
         foreach ($attributes as $attribute => $value) {
             if (empty($value)) {
                 $model->{$attribute} = null;
-            } elseif (Arr::exists($model->{$attribute} ?? [], $attribute)) {
+            } elseif ($model->hasAttribute($attribute)) {
                 $model->{$attribute} = array_values(
-                    array_diff($model->{$attribute}, (array) $value)
+                    array_diff($model->{$attribute}, (array)$value)
                 );
             }
         }
@@ -104,7 +104,7 @@ trait EmulatesModelQueries
     /**
      * Parse the database query results.
      *
-     * @param  \Illuminate\Database\Eloquent\Collection  $resource
+     * @param \Illuminate\Database\Eloquent\Collection $resource
      */
     public function parse(mixed $resource): array
     {
