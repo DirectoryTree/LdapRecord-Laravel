562fde4cd5d1176c2881ebec37db6e07536ad742
Steve Bauman <steven_bauman@outlook.com>
2025-02-25 19:38:38 -0500

    Allow Laravel to generate timestamps on migration

--- database/migrations/2025_01_01_000000_add_ldap_columns_to_users_table.php
+++ database/migrations/2025_01_01_000000_add_ldap_columns_to_users_table.php
@@ -1,18 +1,16 @@
 <?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
-class AddLdapColumnsToUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     * 
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         $driver = Schema::getConnection()->getDriverName();
 
@@ -34,10 +32,8 @@ class AddLdapColumnsToUsersTable extends Migration
 
     /**
      * Reverse the migrations.
-     * 
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['guid', 'domain']);
@@ -46,13 +42,8 @@ class AddLdapColumnsToUsersTable extends Migration
 
     /**
      * Compile a compatible "unique" SQL Server index constraint.
-     * 
-     * @param string $table
-     * @param string $column 
-     * 
-     * @return string 
      */
-    protected function compileUniqueSqlServerIndexStatement($table, $column)
+    protected function compileUniqueSqlServerIndexStatement(string $table, string $column): string
     {
         return sprintf('create unique index %s on %s (%s) where %s is not null',
             implode('_', [$table, $column, 'unique']),
@@ -61,4 +52,4 @@ class AddLdapColumnsToUsersTable extends Migration
             $column
         );
     }
-}
+};
