28430395029c8e956b66d94dfbd1d0b9b5a49c38
Steve Bauman <steven_bauman@outlook.com>
2026-03-28 14:09:46 -0400

    Remove unused model method and small refactors

--- src/Testing/EmulatedBuilder.php
+++ src/Testing/EmulatedBuilder.php
@@ -3,60 +3,21 @@
 namespace LdapRecord\Laravel\Testing;
 
 use Illuminate\Support\Arr;
-use LdapRecord\Models\Model;
-use LdapRecord\Models\Types\ActiveDirectory;
-use LdapRecord\Models\Types\OpenLDAP;
 use LdapRecord\Query\Builder;
-use LdapRecord\Query\Model\Builder as ModelBuilder;
 
 class EmulatedBuilder extends Builder
 {
     use EmulatesQueries;
 
     /**
-     * Create a new Eloquent model builder.
-     *
-     * @return mixed
-     */
-    public function model(Model $model): ModelBuilder
-    {
-        $builder = $this->determineBuilderFromModel($model);
-
-        return (new $builder($model, $this))->setBaseDn($this->baseDn);
-    }
-
-    /**
-     * Determine the query builder to use for the model.
-     */
-    protected function determineBuilderFromModel(Model $model): string
-    {
-        switch (true) {
-            case $model instanceof ActiveDirectory:
-                return Emulated\ActiveDirectoryBuilder::class;
-            case $model instanceof OpenLDAP:
-                return Emulated\OpenLdapBuilder::class;
-            default:
-                return Emulated\ModelBuilder::class;
-        }
-    }
-
-    /**
      * Process the database query results into an LDAP result set.
      */
     protected function process(array $results): array
     {
-        return array_map([$this, 'mergeAttributesAndTransformResult'], $results);
-    }
-
-    /**
-     * Merge  and transform the result.
-     */
-    protected function mergeAttributesAndTransformResult(array $result): array
-    {
-        return array_merge(
+        return array_map(fn (array $result) => array_merge(
             $this->transform($result),
             $this->retrieveExtraAttributes($result)
-        );
+        ), $results);
     }
 
     /**
